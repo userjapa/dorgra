c0007815277be02c85156e8ce7f678f45736ee36
usuarioJapa <willian.hsimabukuro@gmail.com>
2020-08-30 04:07:50 -0300

    removed return

--- server/index.js
+++ server/index.js
@@ -4,27 +4,24 @@ const apiRoute = require('./route/api')
 
 module.exports = (req, res) => {
   if (req.url == '/favicon.ico') {
-    return fs.readFile('./favicon.ico', (err, data) => {
+    fs.readFile('./favicon.ico', (err, data) => {
       if (err) {
         res.write('Error')
-        return res.end()
+        res.end()
       }
 
       res.end(data);
     })
   } else if (req.url.match('/api') && req.url.match('/api').index == 0) {
-     return apiRoute(req, res)
+     apiRoute(req, res)
   } else {
-    return fs.readFile('./public/index.html', (err, data) => {
+    fs.readFile('./public/index.html', (err, data) => {
       if (err) {
         res.write('Error')
-        return res.end()
+        res.end()
       }
 
       res.end(data);
     })
   }
-
-  res.write('API valida boleto')
-  res.end()
 }
